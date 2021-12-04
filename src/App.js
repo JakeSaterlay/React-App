6461febbdbee86551de0dd617adec6afc781c16d
Jake Saterlay <jake@vocala.co>
2021-12-04 23:13:44 +0000

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello from the mac!!
+          Learn React hello from the pc!
         </a>
       </header>
     </div>

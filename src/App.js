bd13af7e7a5b24ccad207567a508800c22025ff9
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2021-12-04 00:56:47 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React hello!
+          Learn React hello from the mac!!
         </a>
       </header>
     </div>

8c19f88264e2444be62a1de217913651ed576186
Jake Saterlay <jake@vocala.co>
2022-02-26 20:12:04 +0000

    lottie

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,7 @@
 import linkedin from './images/linkedin.png';
 import github from './images/github.png';
+import Lottie from 'react-lottie';
+import penguin from './images/penguin.json';
 import './App.css';
 import React from 'react';
 import Modal from 'react-modal';
@@ -15,6 +17,16 @@ const customStyles = {
   },
 };
 
+function lottieOptions(fileName) {
+    return {
+    loop: true,
+    autoplay: true, 
+    animationData: fileName,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    } 
+  }
+}
 
 function Home() {
   let subtitle;
@@ -46,42 +58,47 @@ function Home() {
 
   return (
         <div>
-          <h1>{state.title}</h1>
+          {/* <h1>{state.title}</h1> */}
           <p className="header-title">
             Jake Saterlay Portfolio
           </p>
-          <button onClick={changeTitle}>CLICK ME</button>
+          {/* <button onClick={changeTitle}>CLICK ME</button> */}
           <div className="App-header">
-            <div className="cards"> 
-              <a className="card" href="https://www.linkedin.com/in/jake-saterlay/" target="_blank">
-                <div>
-                  <img src={linkedin} className="card-img" />
-                </div>
-              </a>
-              <a className="card" href="https://github.com/JakeSaterlay" target="_blank">
-                <div>
-                  <img src={github} className="card-img" />
-                </div>
-              </a>
+            <div className="welcome-content"> 
+            <Lottie options={lottieOptions(penguin)}
+              height={400}
+              width={400}/>
+              <div class="cards">
+                <a className="card" href="https://www.linkedin.com/in/jake-saterlay/" target="_blank">
+                  <div>
+                    <img src={linkedin} className="card-img" />
+                  </div>
+                </a>
+                <a className="card" href="https://github.com/JakeSaterlay" target="_blank">
+                  <div>
+                    <img src={github} className="card-img" />
+                  </div>
+                </a>
+              </div>
             </div>
             <div className="Links">
               <a href="/about">ABOUT ME</a>
             </div>
           </div>
-          <div>
-      <button onClick={openModal}>Open Modal</button>
-      <Modal
-        isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
-        <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
-      </Modal>
-    </div>
+          {/* <div>
+            <button onClick={openModal}>Open Modal</button>
+            <Modal
+              isOpen={modalIsOpen}
+              onAfterOpen={afterOpenModal}
+              onRequestClose={closeModal}
+              style={customStyles}
+              contentLabel="Example Modal"
+            >
+              <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+              <button onClick={closeModal}>close</button>
+              <div>I am a modal</div>
+            </Modal>
+          </div> */}
         </div>
 
   );

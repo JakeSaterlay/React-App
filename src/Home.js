b2cc451b8dcd3dc424e2ab12a6611c954687ee22
Jake Saterlay <jake@vocala.co>
2022-02-27 00:12:21 +0000

    moved state and modal stuff to about page

--- src/Home.js
+++ src/Home.js
@@ -4,18 +4,9 @@ import Lottie from 'react-lottie';
 import penguin from './images/penguin.json';
 import './App.css';
 import React from 'react';
-import Modal from 'react-modal';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+
+
 
 function lottieOptions(fileName) {
     return {
@@ -29,36 +20,9 @@ function lottieOptions(fileName) {
 }
 
 function Home() {
-  let subtitle;
-  const [state, setState] = React.useState({title: "apples"});
-
-  function changeTitle(){
-    if(state.title == "apples"){
-      setState({title: "oranges"})
-    }
-    else {
-      setState({title: "apples"})
-    }
-  }
-
-  const [modalIsOpen, setIsOpen] = React.useState(false);
-
-  function openModal() {
-    setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
-
-  function closeModal() {
-    setIsOpen(false);
-  }
 
   return (
         <div>
-          {/* <h1>{state.title}</h1> */}
           <p className="header-title">
             Jake Saterlay Portfolio
           </p>
@@ -85,20 +49,6 @@ function Home() {
               <a href="/about">ABOUT ME</a>
             </div>
           </div>
-          {/* <div>
-            <button onClick={openModal}>Open Modal</button>
-            <Modal
-              isOpen={modalIsOpen}
-              onAfterOpen={afterOpenModal}
-              onRequestClose={closeModal}
-              style={customStyles}
-              contentLabel="Example Modal"
-            >
-              <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
-              <button onClick={closeModal}>close</button>
-              <div>I am a modal</div>
-            </Modal>
-          </div> */}
         </div>
 
   );

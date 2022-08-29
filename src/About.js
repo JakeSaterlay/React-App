388fe2fd2920198474f6c219181bc918d3ba0e42
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2022-08-29 14:34:30 +0100

    added modal template to aboutme page

--- src/About.js
+++ src/About.js
@@ -3,11 +3,34 @@ import "./About.css";
 import Modal from "react-modal";
 import React from "react";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 function About() {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="about-section">
       <img src={jake} className="card-img" />
-      <p>Hi! My name is Jake Saterlay and I am a programmer from Norwich.</p>
+      <p>
+        Hi! My name is Jake Saterlay and I am a Software Developer from Norwich.
+      </p>
       <p>
         I have experience in C#, .NET Core, HTML, CSS, JavaScript and ReactJS
       </p>
@@ -20,6 +43,22 @@ function About() {
           Mastering React
         </a>
       </div>
+      <a href="#" onClick={openModal}>
+        Projects
+      </a>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <button onClick={closeModal}>close</button>
+        <div>My Projects</div>
+        <div>League of Legends Champion Randomiser, built in ReactJS</div>
+        <div>Harry Potter API built in Node and Express</div>
+        <div>Football API built in C#, .NET Core and MongoDB</div>
+        <div>Front end website for my sister's business, built in ReactJS</div>
+      </Modal>
       <a href="/">Home</a>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,56 +1,54 @@
-import linkedin from './images/linkedin.png';
-import github from './images/github.png';
-import Lottie from 'react-lottie';
-import penguin from './images/penguin.json';
-import './App.css';
-import React from 'react';
-
-
-
+import linkedin from "./images/linkedin.png";
+import github from "./images/github.png";
+import Lottie from "react-lottie";
+import penguin from "./images/penguin.json";
+import "./App.css";
+import React from "react";
 
 function lottieOptions(fileName) {
-    return {
+  return {
     loop: true,
-    autoplay: true, 
+    autoplay: true,
     animationData: fileName,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    } 
-  }
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 }
 
 function Home() {
-
   return (
-        <div>
-          <p className="header-title">
-            Jake Saterlay Portfolio
-          </p>
-          {/* <button onClick={changeTitle}>CLICK ME</button> */}
-          <div className="App-header">
-            <div className="welcome-content"> 
-            <Lottie options={lottieOptions(penguin)}
-              height={400}
-              width={400}/>
-              <div class="cards">
-                <a className="card" href="https://www.linkedin.com/in/jake-saterlay/" target="_blank">
-                  <div>
-                    <img src={linkedin} className="card-img" />
-                  </div>
-                </a>
-                <a className="card" href="https://github.com/JakeSaterlay" target="_blank">
-                  <div>
-                    <img src={github} className="card-img" />
-                  </div>
-                </a>
+    <div>
+      <p className="header-title">Jake Saterlay Portfolio</p>
+      <div className="App-header">
+        <div className="welcome-content">
+          <Lottie options={lottieOptions(penguin)} height={400} width={400} />
+          <div class="cards">
+            <a
+              className="card"
+              href="https://www.linkedin.com/in/jake-saterlay/"
+              target="_blank"
+            >
+              <div>
+                <img src={linkedin} className="card-img" />
               </div>
-            </div>
-            <div className="Links">
-              <a href="/about">ABOUT ME</a>
-            </div>
+            </a>
+            <a
+              className="card"
+              href="https://github.com/JakeSaterlay"
+              target="_blank"
+            >
+              <div>
+                <img src={github} className="card-img" />
+              </div>
+            </a>
           </div>
         </div>
-
+        <div className="Links">
+          <a href="/about">ABOUT ME</a>
+        </div>
+      </div>
+    </div>
   );
 }
 
